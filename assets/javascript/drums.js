86c1eb23e9afac431173d252b07628cc66a7bf29
jmihalsky <jmihalsky@gmail.com>
2018-12-25 21:52:14 -0800

    updated code

--- assets/javascript/drums.js
+++ assets/javascript/drums.js
@@ -6,24 +6,36 @@ function drum_key(key_val){
             audio.play();
             break;
         case 83:
-            var audioa = new Audio("./assets/drum_sounds/hihat.wav");
-            audioa.play();
+            var audio = new Audio("./assets/drum_sounds/hihat.wav");
+            audio.play();
             break;
         case 68:
             var audio = new Audio("./assets/drum_sounds/kick.wav");
             audio.play();
             break;
         case 70:
+            var audio = new Audio("./assets/drum_sounds/openhat.wav");
+            audio.play();
             break;
         case 71:
+            var audio = new Audio("./assets/drum_sounds/boom.wav");
+            audio.play();
             break;
         case 72:
+            var audio = new Audio("./assets/drum_sounds/ride.wav");
+            audio.play();
             break;
         case 74:
+            var audio = new Audio("./assets/drum_sounds/snare.wav");
+            audio.play();
             break;
         case 75:
+            var audio = new Audio("./assets/drum_sounds/tom.wav");
+            audio.play();
             break;
         case 76:
+            var audio = new Audio("./assets/drum_sounds/tink.wav");
+            audio.play();
             break;
     }
 }
